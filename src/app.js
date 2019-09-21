57078a3743c5f0ee44f31e3c35c48cba75328118
Sebastian Meza <sbstnmz@gmail.com>
2019-09-21 20:01:52 -0300

    added react-router and basic config

--- src/app.js
+++ src/app.js
@@ -1,6 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p> This is my boilerplate</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+  <div>
+    Hola mundo desde la raíz de las rutas
+  </div>
+)
+
+const AddPage = () => (
+  <div>
+    Crearr.....
+  </div>
+)
+
+// edit
+// help
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
