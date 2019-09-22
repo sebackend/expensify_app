c05e89abf69313c3754c79f604af1b14e5349f34
Sebastian Meza <sbstnmz@gmail.com>
2019-09-22 01:02:02 -0300

    extra routes with basic custom components

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -8,23 +8,44 @@ const ExpenseDashboardPage = () => (
   <div>
     Hola mundo desde la raíz de las rutas
   </div>
-)
+);
 
 const AddPage = () => (
   <div>
     Crearr.....
   </div>
-)
+);
+
+const EditPage = () => (
+  <div>
+    Editar!
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    Aiuuudaaa
+  </div>
+);
+
+const NotfoundPage = () => (
+  <div>
+    404 - <Link to="/">Go Home</Link>
+  </div>
+);
 
 // edit
 // help
 
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={ExpenseDashboardPage} exact={true} />
       <Route path="/create" component={AddPage} />
-    </div>
+      <Route path="/edit" component={EditPage} />
+      <Route path="/help" component={HelpPage} />
+      <Route component={NotfoundPage} />
+    </Switch>
   </BrowserRouter>
 );
 
