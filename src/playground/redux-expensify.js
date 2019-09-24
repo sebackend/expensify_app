bad3f6f6a5d6427d55e6f550ee8c776eba818c45
Sebastian Meza <sbstnmz@gmail.com>
2019-09-23 23:12:20 -0300

    basic add & remove expensify to store

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,7 +1,30 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
 
 // ADD_EXPENSE
+const addExpense = (
+  { description= '', 
+    note = '', 
+    amount = 0, 
+    createdAt = 0 
+  } = {}
+) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
+
 // REMOVE_EXPENSE
+const removeExpense = ({ id } = {}) => ({
+  type: 'REMOVE_EXPENSE',
+  id
+});
+
 // EDIT_EXPENSE
 // SET_TEXT_FILTER
 // SORT_BY_DATE
@@ -15,6 +38,12 @@ const expensesReducerDefaultState = [];
 // Expenses reducer
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch(action.type){
+    case 'ADD_EXPENSE':
+      return [
+        ...state, action.expense
+      ];
+    case 'REMOVE_EXPENSE':
+      return state.filter(({ id }) => id !== action.id );
     default:
       return state;
   }
@@ -43,20 +72,28 @@ const store = createStore(
   })
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
-const demoState = {
-  expenses: [{
-    id: 'qwerty',
-    description: 'January rent',
-    note: 'This was the final payment for that address...',
-    amount: 54500,
-    createdAt: 0
-  }],
-  filters: {
-    text: 'rent',
-    sortBy: 'date', // date or amunt
-    startDate: undefined,
-    endDate: undefined
-  }
-};
+const expenseOne    = store.dispatch(addExpense({ description: 'Uno', amount: 500 }));
+const expenseTwo    = store.dispatch(addExpense({ description: 'Dos', amount: 1000 }));
+const expenseThree  = store.dispatch(addExpense({ description: 'Tres', amount: 250 }));
+
+store.dispatch(removeExpense({ id: expenseTwo.expense.id }));
+
+// const demoState = {
+//   expenses: [{
+//     id: 'qwerty',
+//     description: 'January rent',
+//     note: 'This was the final payment for that address...',
+//     amount: 54500,
+//     createdAt: 0
+//   }],
+//   filters: {
+//     text: 'rent',
+//     sortBy: 'date', // date or amunt
+//     startDate: undefined,
+//     endDate: undefined
+//   }
+// };
