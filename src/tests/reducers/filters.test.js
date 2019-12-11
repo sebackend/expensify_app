c1502a693a5d7b12ba9dfee8158309d9abb78a21
Sebastian Meza <sbstnmz@gmail.com>
2019-12-10 23:49:15 -0300

    testing filters reducer

---
+++ src/tests/reducers/filters.test.js
@@ -0,0 +1,33 @@
+import moment from 'moment';
+import filtersReducer from '../../reducers/filters';
+
+test('should setup default filter values', () => {
+  const state = filtersReducer(undefined, { type: '@@INIT'});
+
+  expect(state).toEqual({
+    text: '',
+    sortBy: 'date',
+    startDate: moment().startOf('month'),
+    endDate: moment().endOf('month')
+  });
+});
+
+test('should set sortBy amount', () => {
+  const state = filtersReducer(undefined, { type: 'SORT_BY_AMOUNT' });
+
+  expect(state.sortBy).toBe('amount');
+});
+
+test('should set sortBy to date', () => {
+  const currentState = {
+    text: '',
+    sortBy: 'amount',
+    startDate: undefined,
+    endDate: undefined
+  };
+
+  const action = { type: 'SORT_BY_DATE'};
+
+  const state = filtersReducer(currentState, action);
+  expect(state.sortBy).toBe('date');
+});
