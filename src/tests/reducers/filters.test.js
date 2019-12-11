e865a148ede074869d4cae701d08b9f795ee6de3
Sebastian Meza <sbstnmz@gmail.com>
2019-12-10 23:58:11 -0300

    testing additional cases for filters reducer

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -31,3 +31,45 @@ test('should set sortBy to date', () => {
   const state = filtersReducer(currentState, action);
   expect(state.sortBy).toBe('date');
 });
+
+test('should set text filter', () => {
+  const currentState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+  };
+
+  const action = { type: 'SET_TEXT_FILTER', text: 'TEXT EXAMPLE' };
+  const state = filtersReducer(currentState, action);
+
+  expect(state.text).toBe('TEXT EXAMPLE');
+});
+
+test('should set startDate filter', () => {
+  const currentState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+  };
+
+  const action = { type: 'SET_START_DATE', startDate: moment().startOf('year') };
+  const state = filtersReducer(currentState, action);
+
+  expect(state.startDate).toEqual(moment().startOf('year'));
+});
+
+test('should set endDate filter', () => {
+  const currentState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+  };
+
+  const action = { type: 'SET_END_DATE', endDate: moment().startOf('year') };
+  const state = filtersReducer(currentState, action);
+
+  expect(state.endDate).toEqual(moment().startOf('year'));
+});
