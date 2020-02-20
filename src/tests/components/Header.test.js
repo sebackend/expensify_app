6f2bc1f59307f494d60ce93855861c94f6587a5e
Sebastian Meza <sbstnmz@gmail.com>
2020-02-20 16:34:08 -0300

    replace shallow-render with enzyme with custom config files

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
 import Header from '../../components/Pages/Header';
+import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
 
 test('should render Header correctly', () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />); 
+  const wrapper = shallow(<Header />);
 
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
+  expect(toJSON(wrapper)).toMatchSnapshot();
+  // const renderer = new ReactShallowRenderer();
+  // renderer.render(<Header />); 
+
+  // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
